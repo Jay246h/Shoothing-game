dae331435087d09c4c8f8b64b777c04e1bd5898a
Jay246h <babojeon641@gmail.com>
2024-04-07 17:36:04 +0900

    풀링 준비

---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject enemyLPrefab;
+    public GameObject enemyMPrefab;
+    public GameObject enemySPrefab;
+    public GameObject itemCoinPrefab;
+    public GameObject itemPowerPrefab;
+    public GameObject itemBoomPrefab;
+    public GameObject bulletPlayerAPrefab;
+    public GameObject bulletPlayerBPrefab;
+    public GameObject bulletEnemyAPrefab;
+    public GameObject bulletEnemyBPrefab;
+
+    GameObject[] enemyL;
+    GameObject[] enemyM;
+    GameObject[] enemyS;
+
+    GameObject[] itemCoin;
+    GameObject[] itemPower;
+    GameObject[] itemBoom;
+
+    GameObject[] bulletPlayerA;
+    GameObject[] bulletPlayerB;
+    GameObject[] bulletEnemyA;
+    GameObject[] bulletEnemyB;
+
+    void Awake()
+    {
+        enemyL = new GameObject[10];
+        enemyM = new GameObject[10];
+        enemyS = new GameObject[20];
+
+        itemCoin = new GameObject[20];
+        itemPower = new GameObject[20];
+        itemPower = new GameObject[20];
+
+        bulletPlayerA = new GameObject[100];
+        bulletPlayerB = new GameObject[100];
+        bulletEnemyA = new GameObject[100];
+        bulletEnemyB = new GameObject[100];
+
+        Generate();
+    }
+
+    void Generate()
+    {
+        //Enemy
+        for(int index = 0; index < enemyL.Length; index++)
+        {
+            enemyL[index] = Instantiate(enemyLPrefab);
+            enemyL[index].SetActive(false);
+        }
+        for (int index = 0; index < enemyM.Length; index++)
+        {
+            enemyM[index] = Instantiate(enemyMPrefab);
+            enemyM[index].SetActive(false);
+        }
+        for (int index = 0; index < enemyS.Length; index++)
+        {
+            enemyS[index] = Instantiate(enemySPrefab);
+            enemyS[index].SetActive(false);
+        }
+        //item
+        for (int index = 0; index < itemCoin.Length; index++)
+        {
+            itemCoin[index] = Instantiate(itemCoinPrefab);
+            itemCoin[index].SetActive(false);
+        }
+        for (int index = 0; index < itemPower.Length; index++)
+        {
+            itemPower[index] = Instantiate(itemPowerPrefab);
+            itemPower[index].SetActive(false);
+        }
+        for (int index = 0; index < itemBoom.Length; index++)
+        {
+            itemBoom[index] = Instantiate(itemBoomPrefab);
+            itemBoom[index].SetActive(false);
+        }
+        //bullet
+        for (int index = 0; index < bulletPlayerA.Length; index++)
+        {
+            bulletPlayerA[index] = Instantiate(bulletPlayerAPrefab);
+            bulletPlayerA[index].SetActive(false);
+        }
+        for (int index = 0; index < bulletPlayerB.Length; index++)
+        {
+            bulletPlayerB[index] = Instantiate(bulletPlayerBPrefab);
+            bulletPlayerB[index].SetActive(false);
+        }
+        for (int index = 0; index < bulletEnemyA.Length; index++)
+        {
+            bulletEnemyA[index] = Instantiate(bulletEnemyAPrefab);
+            bulletEnemyA[index].SetActive(false);
+        }
+        for (int index = 0; index < bulletEnemyB.Length; index++)
+        {
+            bulletEnemyB[index] = Instantiate(bulletEnemyBPrefab);
+            bulletEnemyB[index].SetActive(false);
+        }
+    }
+}
